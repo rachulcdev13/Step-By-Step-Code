250b06edfb6040dee2c194a42c46e53a12689e2a
rahulcdev <reahulc.dev13@gmail.com>
2022-06-08 04:41:16 +0530

    sasaas

--- src/API/GetApi.js
+++ src/API/GetApi.js
@@ -3,67 +3,122 @@ import { Table, Form, Col, Row, Button } from "react-bootstrap";
 
 const GetApi = () => {
     const [getapi, setGetApi] = useState([]);
-    const [name, setName] = useState('Rahul');
-    const [cityname, setCity] = useState('Pune');
+    const [name, setName] = useState('');
+    const [cityname, setCity] = useState('');
 
+    //Get method
     useEffect(() => { 
-        // run jsone server
-        // json-server --watch db.json --port 8000 
+        getList();
+    }, []); 
+
+    //Once Delte no need to refresh portal using useEffect
+    const getList = () => {
         fetch("http://localhost:8000/Apidata/").then((result) => {
-            result.json().then((response) => {
+            result.json().then((resp) => {
                 // console.log("API",response);
-                setGetApi(response)
+                setGetApi(resp)
             })
         })
-    }, []);
-    // console.log(getapi);
+    } 
 
-    const SaveForm = () => {
+    //Post method
+    const PostApi = () => {
+        // console.log(name, cityname);
+        const apidata = { name, cityname };
+        fetch("http://localhost:8000/Apidata/", 
+        {
+            method: "POST",
+            headers: 
+            {
+                'Accept': "application/json",
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(apidata)
+        }).then((results) => {
+            // console.log(results);
+            results.json().then((resp) => {
+                console.log(resp);
+            })
+        })
+    }
 
+    //Delete method :
+    const DeleteApi = (id) => { 
+        fetch(`http://localhost:8000/Apidata/${id}`, {
+            method: "DELETE",
+        }).then((result) => {
+            result.json().then((resp) => {
+                console.log(resp);
+                getList();
+            })
+        })
     }
 
     return (
         <>
-            <h6> fetch API | GET method :</h6>
-            <Table striped bordered hover size="sm" >
-                <tbody style={{ textAlign: "center" }}>
-                    <tr style={{ fontWeight: "bold" }}>
-                        <td>Name</td>
-                        <td>city</td>
-                    </tr>
-                    {
-                        getapi.map((currVal, ind) => {
-                            return (
-                                <tr key={ind}>
-                                    <td>{currVal.name}</td>
-                                    <td>{currVal.city}</td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </Table>
-            <br />
-            <h6>POST Method in Rest API</h6>
-            <Form>
-                <Row>
-                    <Col>
-                        <Form.Control
-                            placeholder="Name"
-                            value={name}
-                            onChange={(e) => { setName(e.target.value) }}
-                        />
-                    </Col>
-                    <Col>
-                        <Form.Control
-                            value={cityname}
-                            placeholder="City"
-                            onChange={(e) => { setCity(e.target.value) }}
-                        />
-                    </Col>
-                </Row> <br />
-                <Button variant="success" onClick={SaveForm} >Success</Button>
-            </Form>
+            <div>
+                <h6> API</h6>
+                <li>The <strong>fetch() method</strong> is used to send the requests to the server without refreshing the page. It is an alternative to the XMLHttpRequest object.</li>
+                <li><strong>GET Method</strong> is used to request data from a specified resource.</li>
+                <li><strong>POST Method</strong> is used to send data to a server to create/update a resource.</li>
+                <li>For <strong>GET Method</strong> : Data is visible to everyone in the URL</li>
+                <li>For <strong>POST Method</strong> : Data is not displayed in the URL</li>
+                <li>An <strong>PATCH method</strong> is used to update information partially or completely (May be) of already existing resource at server and produces a new version of resource with updated information</li><br />
+
+                <h6 style={{ color: "red" }}> fetch API | GET method :</h6>
+                <Table striped bordered hover size="sm" >
+                    <tbody style={{ textAlign: "center" }}>
+                        <tr style={{ fontWeight: "bold" }}>
+                            <td>ID</td>
+                            <td>Name</td>
+                            <td>City</td>
+                            <td>Action</td>
+                        </tr>
+                        {
+                            getapi.map((currVal, ind) => {
+                                return (
+                                    <tr key={ind}>
+                                        <td>{currVal.id}</td>
+                                        <td>{currVal.name}</td>
+                                        <td>{currVal.cityname}</td>
+                                        <td><Button variant="danger" onClick={() => DeleteApi(currVal.id)}><span>Delete</span></Button>&nbsp;
+                                            {/* <Button onClick={() => SelectUser(currVal.id)}><span>Edit</span></Button> */}
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </Table>
+            </div><br />
+
+            <div>
+                <h6 style={{ color: "red" }}>POST Method in Rest API :</h6>
+                <Form>
+                    <Row>
+                        <Col>
+                            <Form.Control
+                                placeholder="Name"
+                                value={name}
+                                onChange={(e) => { setName(e.target.value) }}
+                            />
+                        </Col>
+                        <Col>
+                            <Form.Control
+                                value={cityname}
+                                placeholder="City"
+                                onChange={(e) => { setCity(e.target.value) }}
+                            />
+                        </Col>
+                    </Row> <br />
+                    <Button variant="success" onClick={PostApi} >Submit</Button>
+                </Form>
+            </div><br />
+
+            <div>
+                <h6>Pre-filled form Data And Update Data With API</h6>
+
+            </div>
         </>
     )
 }
--- src/RouterComponents/HomePage.js
+++ src/RouterComponents/HomePage.js
@@ -8,17 +8,19 @@ const HomePage = () => {
       <hr style={{ border: "2px solid red", marginLeft: "500px", marginRight: "500px" }}></hr><br />
 
       <Card>
-        <Card.Header>Quote</Card.Header>
+        <Card.Header>Redux</Card.Header>
         <Card.Body>
           <blockquote className="blockquote mb-0">
             <p>
-              {' '}
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-              erat a ante.{' '}
+              <li>A redux is a container where you can store your whole application data.</li>
+              <li>So we call it to state Management</li>
+              <li>It does not belong to component state</li>
+              <li><strong style={{color:"red"}}>In Redux application we can only one Store used. </strong></li> 
+              <br />
+              <h6>Redux Architecture </h6>
+              <h6><span style={{color:"green"}}>VIEW</span> [React Component] == <span style={{color:"red"}}>ACTION</span> [Collect Data from component or API] == <span style={{color:"blue"}}>REDUCER</span> [Get Data From Action and send to Store] == <span style={{color:"Orange"}}>STORE</span> [Sate of complete Application] == React View</h6>
             </p>
-            <footer className="blockquote-footer">
-              Someone famous in <cite title="Source Title">Source Title</cite>
-            </footer>
+            
           </blockquote>
         </Card.Body>
       </Card>
